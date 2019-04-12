ab276f1339c3b575c7ec39aad8378ea174a1499f
Eugene <mrshaftoe@gmail.com>
2019-04-13 01:46:50 +0300

    page-header заданы верные размеры для всех разрешений

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -6,3 +6,15 @@ menu.forEach(function(elem) {
   elem.classList.add("site-menu--closed");
 });
 
+toggle.addEventListener("click", function () {
+  menu.forEach(function(elem) {
+    if (elem.classList.contains("site-menu--closed")) {
+      elem.classList.remove("site-menu--closed");
+      elem.classList.add("site-menu--opened");
+    } else {
+      elem.classList.remove("site-menu--opened");
+      elem.classList.add("site-menu--closed");
+    }
+  });
+});
+
