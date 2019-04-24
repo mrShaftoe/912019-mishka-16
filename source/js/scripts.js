b57e404331309436d7e8263a9dd1d4667c8b9f04
Eugene <mrshaftoe@gmail.com>
2019-04-24 20:49:26 +0300

    svg4everybody polifill for IE added

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,13 +1,14 @@
 var toggle = document.querySelector(".site-menu__toggle");
 var menu = document.querySelectorAll(".site-menu");
 
-menu.forEach(function(elem) {
-  elem.classList.remove("site-menu--nojs");
-  elem.classList.add("site-menu--closed");
-});
+for (var i = 0; i < menu.length; i++) {
+  menu[i].classList.remove("site-menu--nojs");
+  menu[i].classList.add("site-menu--closed");
+}
 
 toggle.addEventListener("click", function () {
-  menu.forEach(function(elem) {
+  for (var i = 0; i < menu.length; i++) {
+    var elem = menu[i];
     if (elem.classList.contains("site-menu--closed")) {
       elem.classList.remove("site-menu--closed");
       elem.classList.add("site-menu--opened");
@@ -15,5 +16,5 @@ toggle.addEventListener("click", function () {
       elem.classList.remove("site-menu--opened");
       elem.classList.add("site-menu--closed");
     }
-  });
+  }
 });
